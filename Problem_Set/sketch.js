7574b1d6deccf93287c6f63d811a814e25891578
MistaFish31 <zhang.ma@husky.neu.edu>
2016-02-16 11:04:49 -0500

    init commit
    problems one and two

---
+++ Problem_Set/sketch.js
@@ -0,0 +1,18 @@
+/*ONE*/
+
+var AddMe=function(x,y){
+  return x+y
+}
+
+console.log(AddMe(2,3))
+
+/*TWO*/
+
+var DivideMe=function(x,y){
+  return x/y
+}
+
+console.log(DivideMe(144,12))
+
+/*THREE*/
+
